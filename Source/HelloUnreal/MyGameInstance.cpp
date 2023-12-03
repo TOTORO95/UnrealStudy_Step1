0bf2fc83c7872473469181209088f454a40c8280
TOTORO95 <jyk950105@gmail.com>
2023-12-03 19:04:23 +0900

    TArray, TSet

--- Source/HelloUnreal/MyGameInstance.cpp
+++ Source/HelloUnreal/MyGameInstance.cpp
@@ -2,31 +2,61 @@
 
 #include "MyGameInstance.h"	   //해당 cpp의 header 파일은 언제나 최상위에
 
+#include "Algo/Accumulate.h"
 #include "Card.h"
+#include "CourseInfo.h"
 #include "Person.h"
 #include "Staff.h"
 #include "Student.h"
 #include "Teacher.h"
-#include "CourseInfo.h"
 
 UMyGameInstance::UMyGameInstance()
 {
-	SchoolName = TEXT("Uneral School");
 }
 
 void UMyGameInstance::Init()
 {
 	Super::Init();
-	CourseInfo = NewObject<UCourseInfo>(this);//Outer를 해당 인스턴스로 셋팅함으로써 has-a관계 성립
+	const int32 ArrayNum = 10;
+	TArray<int32> Int32Array;
 
-	TArray<UStudent*> Persons = {NewObject<UStudent>(), NewObject<UStudent>(), NewObject<UStudent>()};
-	
-	for (int32 i = 0; i < Persons.Num(); i++)
+	for (int32 Idx = 1; Idx <= ArrayNum; ++Idx)
 	{
-		FString NewName = TEXT("Student");
-		NewName.AppendInt(i);
-		Persons[i]->SetName(NewName);
-		CourseInfo->OnChanged.AddUObject(Persons[i], &UStudent::GetNotification);
+		Int32Array.Add(Idx);
 	}
-	CourseInfo->ChangedCourseInfo(SchoolName, TEXT("Changed CourseInfo!!!"));
+
+	Int32Array.RemoveAll([](int32 Val) { return Val % 2 == 0; });
+
+	Int32Array += {2, 4, 6, 8, 10};
+
+	TArray<int32> Int32ArrayCompare;
+	int32 CArray[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+	Int32ArrayCompare.AddUninitialized(ArrayNum);
+	FMemory::Memcpy(Int32ArrayCompare.GetData(), CArray, sizeof(int32) * ArrayNum);
+	ensure(Int32Array == Int32ArrayCompare);
+
+	int32 Sum = 0;
+	for (int32 Idx = 0; Idx < Int32Array.Num(); ++Idx)
+	{
+		Sum += Int32Array[Idx];
+	}
+	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
+	ensure(Sum != SumByAlgo);
+
+	TSet<int32> Int32Set;
+	for (int32 Idx = 1; Idx <= ArrayNum; ++Idx)
+	{
+		Int32Set.Add(Idx);
+	}
+
+	Int32Set.Remove(6);
+	Int32Set.Remove(7);
+	Int32Set.Remove(8);
+	Int32Set.Remove(9);
+	Int32Set.Remove(10);
+	Int32Set.Add(6);
+	Int32Set.Add(7);
+	Int32Set.Add(8);
+	Int32Set.Add(9);
+	Int32Set.Add(10);
 }
